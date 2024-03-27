5179eacb975069723e758dd5c2706cd3d49dde3c
Danila Panov <s02210506@gse.cs.msu.ru>
2024-03-28 00:01:29 +0300

    complete all sql scripts

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,32 +1,39 @@
--- docker run -p 5432:5432 —name webprac -e POSTGRES_DB=webprac -e POSTGRES_PASSWORD=1234 -v src/main/resources/sql:/data/ -d postgres
-
 CREATE TABLE individual_client (
   client_id SERIAL PRIMARY KEY,
-  passport CHAR[10] UNIQUE NOT NULL,
-  surname VARCHAR[100] NOT NULL,
-  "name" VARCHAR[100] NOT NULL,
-  patronymic VARCHAR[100],
-  region VARCHAR[100] NOT NULL,
-  contact_phone_number CHAR[12] NOT NULL,
-  email VARCHAR[100]
+  passport CHAR(10) UNIQUE NOT NULL,
+  surname VARCHAR(100) NOT NULL,
+  "name" VARCHAR(100) NOT NULL,
+  patronymic VARCHAR(100),
+  region VARCHAR(100) NOT NULL,
+  contact_phone_number CHAR(12) NOT NULL,
+  email VARCHAR(100)
 );
 
 CREATE TABLE entity_client (
   client_id SERIAL PRIMARY KEY,
-  registration_number CHAR[13] UNIQUE NOT NULL,
-  "name" VARCHAR[100] NOT NULL,
-  region VARCHAR[100] NOT NULL,
+  registration_number CHAR(13) UNIQUE NOT NULL,
+  "name" VARCHAR(100) NOT NULL,
+  region VARCHAR(100) NOT NULL,
   "address" TEXT NOT NULL,
-  contact_surname VARCHAR[100] NOT NULL,
-  contact_name VARCHAR[100] NOT NULL,
-  contact_patronymic VARCHAR[100],
-  contact_phone_number CHAR[12] NOT NULL,
-  email VARCHAR[100]
+  contact_surname VARCHAR(100) NOT NULL,
+  contact_name VARCHAR(100) NOT NULL,
+  contact_patronymic VARCHAR(100),
+  contact_phone_number CHAR(12) NOT NULL,
+  email VARCHAR(100)
 );
 
 CREATE TYPE ACCOUNT_STATUS AS ENUM ('Активен', 'Заблокирован', 'Закрыт');
-CREATE TYPE OPERATION_TYPE AS ENUM ('Пополнение', 'Подключение услуги', 'Абонентская плата', 'Отключение услуги');
-CREATE TYPE SUBSCRIPTION_TYPE AS ENUM ('Без а/п (единовременная)', 'Ежедневно', 'Ежемесячно', 'Ежегодно');
+CREATE TYPE OPERATION_TYPE AS ENUM (
+    'Пополнение',
+    'Подключение услуги',
+    'Абонентская плата',
+    'Отключение услуги',
+    'Открытие счёта',
+    'Блокировка счёта',
+    'Разблокировка счёта',
+    'Закрытие счёта'
+);
+CREATE TYPE SUBSCRIPTION_TYPE AS ENUM ('Без а/п', 'Ежедневно', 'Ежемесячно', 'Ежегодно');
 
 CREATE TABLE account (
   account_id SERIAL PRIMARY KEY,
@@ -34,16 +41,16 @@ CREATE TABLE account (
   entity_client_id INT REFERENCES entity_client (client_id) ON DELETE CASCADE,
   creation_time timestamp NOT NULL,
   balance NUMERIC NOT NULL,
-  serviced_phone_number CHAR[12],
+  serviced_phone_number CHAR(12),
   status ACCOUNT_STATUS NOT NULL,
-  credit_max NUMERIC,
+  credit_max NUMERIC NOT NULL,
   credit_interval INTERVAL,
   CHECK ( (individual_client_id IS NULL) != (entity_client_id IS NULL) )
 );
 
 CREATE TABLE service (
   service_id SERIAL PRIMARY KEY,
-  "name" VARCHAR[100] UNIQUE NOT NULL,
+  "name" VARCHAR(100) UNIQUE NOT NULL,
   subscription_type SUBSCRIPTION_TYPE NOT NULL,
   activation_fee NUMERIC,
   subscription_fee NUMERIC,
@@ -54,34 +61,126 @@ CREATE TABLE service (
   package_message SMALLINT,     -- in pieces
   description TEXT,
   CONSTRAINT subscription_type_check CHECK (
-    (subscription_type = 'Без а/п (единовременная)') OR NOT (activation_fee IS NOT NULL AND duration IS NULL) AND
-    (subscription_type IN ('Ежедневно', 'Ежемесячно', 'Ежегодно')) OR NOT (subscription_fee IS NOT NULL)
+    NOT (subscription_type = 'Без а/п') OR (activation_fee IS NOT NULL) AND
+    NOT (subscription_type IN ('Ежедневно', 'Ежемесячно', 'Ежегодно')) OR (subscription_fee IS NOT NULL)
   )
 );
+
+CREATE TABLE connected_service (
+  connection_id SERIAL PRIMARY KEY,     -- do i need it?
+  account_id INT NOT NULL REFERENCES account (account_id) ON DELETE CASCADE,
+  service_id INT NOT NULL REFERENCES service (service_id) ON DELETE CASCADE,
+  connection_time TIMESTAMP NOT NULL,
+  UNIQUE (account_id, service_id, connection_time)
+);
+
 
 CREATE TABLE operation (
   operation_id SERIAL PRIMARY KEY,
   operation_time TIMESTAMP NOT NULL,
   account_id INT NOT NULL REFERENCES account (account_id) ON DELETE CASCADE,
   "type" OPERATION_TYPE NOT NULL,
-  money_amount NUMERIC NOT NULL CHECK (money_amount >= 0),
+  money_amount NUMERIC,
   service_id INT REFERENCES service (service_id) ON DELETE CASCADE,
   UNIQUE (operation_time, account_id, service_id),
-  CHECK ( ("type" = 'Пополнение') = (service_id IS NULL) )
+  CHECK ( ("type" IN ('Подключение услуги','Абонентская плата','Отключение услуги')) = (service_id IS NOT NULL) )
 );
 
-CREATE TABLE connected_service (
-  connection_id SERIAL PRIMARY KEY,
-  account_id INT NOT NULL REFERENCES account (account_id) ON DELETE CASCADE,
-  service_id INT NOT NULL REFERENCES service (service_id) ON DELETE CASCADE,
-  connection_time TIMESTAMP NOT NULL,
-  disconnection_time TIMESTAMP,
-  CHECK (disconnection_time IS NULL OR disconnection_time > connection_time),
-  UNIQUE (account_id, service_id, connection_time)
-);
+-- -- -- TRIGGERS -- -- --
+-- account --
 
+CREATE FUNCTION check_account_status() RETURNS trigger AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT' AND NEW.status <> 'Активен') THEN
+            RAISE 'Невозможно создать новый счёт, статус которого %', NEW.status;
+        END IF;
 
+        IF (TG_OP = 'UPDATE' AND OLD.status = 'Закрыт') THEN
+            RAISE 'Невозможно произвести действия с закрытым счётом';
+        END IF;
 
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER account_manipulation
+    BEFORE INSERT OR UPDATE OF status ON account
+    FOR EACH ROW
+    EXECUTE FUNCTION check_account_status();
 
+CREATE FUNCTION log_account_status() RETURNS trigger AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO operation VALUES
+                (DEFAULT, NEW.creation_time, NEW.account_id, 'Открытие счёта', NULL, NULL);
+        ELSIF (NEW.status != OLD.status) THEN
+            IF (NEW.status = 'Заблокирован') THEN
+                INSERT INTO operation VALUES
+                    (DEFAULT, NOW(), NEW.account_id, 'Блокировка счёта', NULL, NULL);
+            ELSIF (NEW.status = 'Закрыт') THEN
+                INSERT INTO operation VALUES
+                    (DEFAULT, NOW(), NEW.account_id, 'Закрытие счёта', NULL, NULL);
+            ELSIF (NEW.status = 'Активен') THEN
+                INSERT INTO operation VALUES
+                    (DEFAULT, NOW(), NEW.account_id, 'Разблокировка счёта', NULL, NULL);
+             END IF;
+        END IF;
 
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION log_account_balance() RETURNS trigger AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT' AND NEW.balance <> 0) THEN
+            INSERT INTO operation VALUES
+                (DEFAULT, NEW.creation_time, NEW.account_id, 'Пополнение', NEW.balance, NULL);
+        END IF;
+
+        IF (TG_OP = 'UPDATE' AND NEW.balance - OLD.balance > 0) THEN
+            INSERT INTO operation VALUES
+                (DEFAULT, NOW(), NEW.account_id, 'Пополнение', NEW.balance - OLD.balance, NULL);
+        END IF;
+
+        IF (NEW.balance < -NEW.credit_max) THEN
+            UPDATE account SET status = 'Заблокирован' WHERE account_id = NEW.account_id;
+        END IF;
+
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER account_manipulation_1_status
+    AFTER INSERT OR UPDATE OF status ON account
+    FOR EACH ROW
+    EXECUTE FUNCTION log_account_status();
+
+CREATE TRIGGER account_manipulation_2_balance
+    AFTER INSERT OR UPDATE OF balance ON account
+    FOR EACH ROW
+    EXECUTE FUNCTION log_account_balance();
+
+-- connected services --
+
+CREATE FUNCTION log_service_connection() RETURNS trigger AS $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO operation VALUES
+                (DEFAULT, NEW.connection_time, NEW.account_id, 'Подключение услуги', (SELECT activation_fee FROM service WHERE service_id = NEW.service_id), NEW.service_id);
+            UPDATE account SET balance = balance - (SELECT activation_fee FROM service WHERE service_id = NEW.service_id) WHERE account_id = NEW.account_id;
+        END IF;
+
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO operation VALUES
+                (DEFAULT, NOW(), OLD.account_id, 'Отключение услуги', (SELECT deactivation_fee FROM service WHERE service_id = OLD.service_id), OLD.service_id);
+            UPDATE account SET balance = balance - (SELECT deactivation_fee FROM service WHERE service_id = OLD.service_id) WHERE account_id = OLD.account_id;
+        END IF;
+
+        RETURN OLD;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER service_connection
+    AFTER INSERT OR DELETE ON connected_service
+    FOR EACH ROW
+    EXECUTE FUNCTION log_service_connection();
