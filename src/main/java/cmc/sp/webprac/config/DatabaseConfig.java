0a0c1a91484d5ed104e8abb9736f41437180c48b
Danila Panov <s02210506@gse.cs.msu.ru>
2024-03-28 21:28:47 +0300

    hibernate properties are now fetched from application.properties file and not from xml

--- src/main/java/cmc/sp/webprac/config/DatabaseConfig.java
+++ src/main/java/cmc/sp/webprac/config/DatabaseConfig.java
@@ -1,16 +1,51 @@
 package cmc.sp.webprac.config;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DatabaseConfig {
+    @Value("${hibernate.connection.driver_class}")
+    private String driver_class;
+    @Value("${hibernate.connection.url}")
+    private String url;
+    @Value("${hibernate.connection.username}")
+    private String username;
+    @Value("${hibernate.connection.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver_class);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
     @Bean
     public LocalSessionFactoryBean sessionFactoryBean() {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setConfigLocation(new ClassPathResource("hibernate.cfg.xml"));
+        sessionFactoryBean.setDataSource(this.getDataSource());
+        sessionFactoryBean.setPackagesToScan("cmc.sp.webprac.models");
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.highlight_sql", "true");
+
+        sessionFactoryBean.setHibernateProperties(properties);
         return sessionFactoryBean;
     }
 }
