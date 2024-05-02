c8dd93af04bec30dfe5939a6c7e746314ad5dcb5
Danila Panov <s02210506@gse.cs.msu.ru>
2024-05-02 13:39:37 +0300

    add base class for clients, modify clients controller

--- src/main/java/cmc/sp/webprac/controllers/ClientsController.java
+++ src/main/java/cmc/sp/webprac/controllers/ClientsController.java
@@ -1,16 +1,36 @@
 package cmc.sp.webprac.controllers;
 
+import cmc.sp.webprac.dao.EntityClientDAO;
+import cmc.sp.webprac.dao.IndividualClientDAO;
+import cmc.sp.webprac.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("/clients")
 public class ClientsController {
 
+    @Autowired
+    IndividualClientDAO individualClient;
+
+    @Autowired
+    EntityClientDAO entityClient;
+
     @GetMapping("")
     String clients(Model model) {
+        var all_clients = new ArrayList<Client>();
+        for (var client : individualClient.getAll()) {
+            all_clients.add(new Client(client));
+        }
+        for (var client : entityClient.getAll()) {
+            all_clients.add(new Client(client));
+        }
+        model.addAttribute("clients", all_clients);
         return "clients";
     }
 
---
+++ src/main/java/cmc/sp/webprac/models/Client.java
@@ -0,0 +1,115 @@
+package cmc.sp.webprac.models;
+
+import java.util.List;
+
+public class Client {
+    private Integer client_id;
+    private ClientType type;
+    private String name;
+    private String registration_number;
+    private String region;
+    private String contact_phone_number;
+    private String email;
+    private List<Account> accounts;
+
+    public Client() {
+    }
+
+    enum ClientType {
+        INDIVIDUAL("физ"),
+        ENTITY("юр");
+        private final String value;
+
+        ClientType(String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return value;
+        }
+    }
+
+    public Client(IndividualClient individualClient) {
+        this.client_id = individualClient.getClient_id();
+        this.type = ClientType.INDIVIDUAL;
+        this.name = individualClient.getFullName();
+        this.registration_number = individualClient.getPassport();
+        this.region = individualClient.getRegion();
+        this.contact_phone_number = individualClient.getContact_phone_number();
+        this.email = individualClient.getEmail();
+        this.accounts = individualClient.getAccounts();
+    }
+
+    public Client(EntityClient entityClient) {
+        this.client_id = entityClient.getClient_id();
+        this.type = ClientType.ENTITY;
+        this.name = entityClient.getName();
+        this.registration_number = entityClient.getRegistration_number();
+        this.region = entityClient.getRegion();
+        this.contact_phone_number = entityClient.getContact_phone_number();
+        this.email = entityClient.getEmail();
+        this.accounts = entityClient.getAccounts();
+    }
+
+    public String getType() {
+        return type.value();
+    }
+
+    public Integer getClient_id() {
+        return client_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegistration_number() {
+        return registration_number;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getContact_phone_number() {
+        return contact_phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setClient_id(Integer client_id) {
+        this.client_id = client_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegistration_number(String registration_number) {
+        this.registration_number = registration_number;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setContact_phone_number(String contact_phone_number) {
+        this.contact_phone_number = contact_phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+
+}
